acf55105540051d8cc6819a84216460bed13650d
akeshpatil@gmail.com <akeshpatil@gmail.com>
2015-11-16 12:08:41 +0200

    updated recipes

--- recipes/applayer.rb
+++ recipes/applayer.rb
@@ -3,12 +3,6 @@
 # Recipe:: applayer
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
-maven 'spring-context' do
-  group_id 'org.springframework'
-  version  '4.0.4.RELEASE'
-  dest     '/root/chef-repo/'
-  repositories ['http://repo.maven.apache.org/maven2/']
-end
 
 file '/tmp/appfile.txt' do
   content 'This is app layer'
--- recipes/default.rb
+++ recipes/default.rb
@@ -4,9 +4,3 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
-maven 'spring-context' do
-  group_id 'org.springframework'
-  version  '4.0.4.RELEASE'
-  dest     '/root/chef-repo/'
-  repositories ['http://repo.maven.apache.org/maven2/']
-end
